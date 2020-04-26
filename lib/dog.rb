9ccd22ce7c70c66ff5a5752d35dcce21816f7466
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 23:53:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,11 +28,18 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-    INSERT INTO dogs(name, breed) VALUES (?, ?)
-    SQL
-    dog = DB[:conn].execute(sql, name, breed)
-    dog
+    def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
   end
 
   def self.create
