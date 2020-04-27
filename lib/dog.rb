c29a2d840660118a8326a80b537bae514ff61eeb
Nicholas Keller <nic.keller328@gmail.com>
2020-04-27 00:14:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,7 +86,16 @@ class Dog
   end
 
   def find_by_name(dog_name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
