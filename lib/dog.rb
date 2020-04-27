0c7cb4b3f8873d8657cedeb2824371ad9e11e5d3
Nicholas Keller <nic.keller328@gmail.com>
2020-04-27 00:17:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -98,7 +98,12 @@ class Dog
   end
 
   def update
-    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    sql = <<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?
+    WHERE id = ?
+    SQL
+
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
