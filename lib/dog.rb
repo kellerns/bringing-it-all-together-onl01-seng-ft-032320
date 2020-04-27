5751b8c24c6150146f85356ea6c3fe62d535977a
Nicholas Keller <nic.keller328@gmail.com>
2020-04-27 00:13:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
 
     dog = DB[:conn].execute(sql, name, breed).first
 
-    if !dog.empty?
+    if dog
       new_dog = self.new_from_db(dog)
     else
       new_dog = self.create(name: name, breed: breed)
